c0098d285ec8dd2afd0d1698f6960711e240a820
ANDREW <awade75009@gmail.com>
2023-05-03 13:34:25 -0500

    added more functions, no need to use !r

--- cogs/waddermodmail.py
+++ cogs/waddermodmail.py
@@ -4,6 +4,8 @@ import asyncio
 from nextcord.ext import commands
 from nextcord.ext.commands import has_permissions, MissingPermissions, BadArgument
 import os
+import re
+
 
 mails_channel_name = "wmodmail"
 transcripts_folder = "transcripts"
@@ -13,8 +15,6 @@ class ModMail(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.conversations = {}  # Stores conversation messages
-
-    
 
     async def save_transcript(self, user_id):
             if user_id in self.conversations:
@@ -35,7 +35,6 @@ class ModMail(commands.Cog):
         else:
             print(f"Transcript not found at {file_path}")  # Debug print
             return None
-
 
     @commands.command(description="Sets up Wadder ModMail")
     @commands.guild_only()
@@ -93,151 +92,183 @@ class ModMail(commands.Cog):
         #     )
         #     await ctx.send(embed=embed)
 
+    
     @commands.Cog.listener()
     async def on_message(self, message):
-        if not message.content.startswith('[]'):
-            if isinstance(message.channel, nextcord.channel.DMChannel) and message.author != self.bot.user:
-                user_id = message.author.id
-                if user_id not in self.conversations:
-                    self.conversations[user_id] = []
+        if message.author == self.bot.user:
+            return
 
-                self.conversations[user_id].append((message.author, message.content))
+        if isinstance(message.channel, nextcord.channel.DMChannel):
+            user_id = message.author.id
+            if user_id not in self.conversations:
+                self.conversations[user_id] = []
+            # ... rest of your existing code for handling DMs ...
+
+            if user_id not in self.conversations:
+                self.conversations[user_id] = []
+
+            self.conversations[user_id].append((message.author, message.content))
+
+            # Create or get the user-specific channel
+            guild = self.bot.get_guild(1101158751576264795)  # Replace with your guild/server ID
+            channel_name = f'modmail-{user_id}'
+            mails_channel = nextcord.utils.get(guild.text_channels, name=channel_name)
+
+            if not mails_channel:
+                overwrites = {
+                    guild.default_role: nextcord.PermissionOverwrite(read_messages=False)
+                }
+                mails_channel = await guild.create_text_channel(channel_name, overwrites=overwrites)
+
+            try:
+                # send to server
+                embed_to = nextcord.Embed(
+                    title='📨 Mod Mail Received',
+                    color=nextcord.Color.orange(),
+                    timestamp=message.created_at
+                )
+                embed_to.set_author(name=f"{message.author}", icon_url=message.author.avatar.url)
+                embed_to.add_field(name='**User ID**', value=f"`{message.author.id}`", inline=False)
+                embed_to.add_field(name='**Message**', value=f"```{message.content}```", inline=False)
+                embed_to.set_footer(text="Type your response here", icon_url=self.bot.user.avatar.url)
+                embed_to.set_thumbnail(url=message.author.avatar.url)
+
+                await mails_channel.send(embed=embed_to)
+
+                # report result to user
+                embed_reply = nextcord.Embed(
+                    title='✅ Mail Sent Successfully',
+                    description="Your message has been sent to moderators. They'll get back to you soon.",
+                    color=nextcord.Color.green(),
+                    timestamp=message.created_at
+                )
+                embed_reply.set_author(name=f"{message.author}", icon_url=message.author.avatar.url)
+                embed_reply.set_thumbnail(url=self.bot.user.avatar.url)
+                await message.author.send(embed=embed_reply)
+            except:
+                # report result to user
+                embed_reply = nextcord.Embed(
+                    title='❌ Oops! Something went wrong',
+                    description="I couldn't send your message to moderators. Please try again later.",
+                    color=nextcord.Color.red(),
+                    timestamp=message.created_at
+                )
+                embed_reply.set_author(name=f"{message.author}", icon_url=message.author.avatar.url)
+                embed_reply.set_thumbnail(url=self.bot.user.avatar.url)
+                await message.author.send(embed=embed_reply)
+
+        elif message.channel.name.startswith("modmail-"):
+            try:
+                user_id = int(message.channel.name.split("-")[1])
+            except ValueError:
+                # Invalid channel name format, ignore
+                return
+
+            user = self.bot.get_user(user_id)
+            if not user:
+                return  # User not found
+
+            if message.author.guild_permissions.administrator:
                 try:
-                    # send to server
-                    embed_to = nextcord.Embed(
-                        title='📨 Mod Mail Received',
-                        color=nextcord.Color.orange(),
+                    # send reply to user
+                    embed_reply = nextcord.Embed(
+                        title='🔔 Reply from Moderator',
+                        description=f"```{message.content}```",
+                        color=nextcord.Color.blue(),
                         timestamp=message.created_at
                     )
-                    embed_to.set_author(name=f"{message.author}", icon_url=message.author.avatar.url)
-                    embed_to.add_field(name='**User ID**', value=f"`{message.author.id}`", inline=False)
-                    embed_to.add_field(name='**Message**', value=f"```{message.content}```", inline=False)
-                    embed_to.set_footer(text="use !r <userid> <message>", icon_url=self.bot.user.avatar.url)
-                    embed_to.set_thumbnail(url=message.author.avatar.url)
+                    embed_reply.set_footer(text=f"Sent by {message.author}", icon_url=message.author.avatar.url)
+                    await user.send(embed=embed_reply)
 
-                    mails_channel = self.bot.get_channel(1099582072840003625)
-                    await mails_channel.send(embed=embed_to)
-
-                    # report result to user
-                    embed_reply = nextcord.Embed(
-                        title='✅ Mail Sent Successfully',
-                        description="Your message has been sent to moderators. They'll get back to you soon.",
+                    # send confirmation to moderator
+                    embed_confirm = nextcord.Embed(
+                        title='✅ Reply sent successfully',
+                        description=f"Your reply has been sent to {user.name}",
                         color=nextcord.Color.green(),
                         timestamp=message.created_at
                     )
-                    embed_reply.set_author(name=f"{message.author}", icon_url=message.author.avatar.url)
-                    embed_reply.set_thumbnail(url=self.bot.user.avatar.url)
-                    await message.author.send(embed=embed_reply)
+                    embed_confirm.set_thumbnail(url=self.bot.user.avatar.url)
+                    await message.channel.send(embed=embed_confirm)
                 except:
-                    # report result to user
-                    embed_reply = nextcord.Embed(
+                    # send error message to moderator
+                    embed_error = nextcord.Embed(
                         title='❌ Oops! Something went wrong',
-                        description="I couldn't send your message to moderators. Please try again later.",
+                        description="I couldn't send your message to the user. Please try again later.",
                         color=nextcord.Color.red(),
                         timestamp=message.created_at
                     )
-                    embed_reply.set_author(name=f"{message.author}", icon_url=message.author.avatar.url)
-                    embed_reply.set_thumbnail(url=self.bot.user.avatar.url)
-                    await message.author.send(embed=embed_reply)
-
-
-
-       
-
-
+                    embed_error.set_thumbnail(url=self.bot.user.avatar.url)
+                    await message.channel.send(embed=embed_error)
+            else:
+                # send error message to moderator
+                embed_error = nextcord.Embed(
+                    title='❌ Access denied',
+                    description="You do not have permission to send a message to this user.",
+                    color=nextcord.Color.red(),
+                    timestamp=message.created_at
+                )
+                embed_error.set_thumbnail(url=self.bot.user.avatar.url)
+                await message.channel.send(embed=embed_error)
+    
+    
     @commands.command()
     @commands.guild_only()
     @has_permissions(manage_messages=True)
-    async def end(self, ctx, user_id: int):
+    async def end(self, ctx, user_id: int, channel_id: int):
         user = self.bot.get_user(user_id)
-        if user is not None:
-            await self.save_transcript(user_id)
-            transcript = await self.load_transcript(user_id)
-            if transcript:
-                transcript_file = io.BytesIO(transcript.encode('utf-8'))
-                file = nextcord.File(transcript_file, filename=f"{user_id}_transcript.txt")
-                embed = nextcord.Embed(
-                    title='✅ Transcript saved',
-                    description=f"Conversation with {user.name} has been saved and closed.",
-                    color=nextcord.Color.green(),
-                    timestamp=ctx.message.created_at
-                )
-                await ctx.send(embed=embed, file=file)
+        transcript_channel = self.bot.get_channel(channel_id)
+        if user is not None and transcript_channel is not None:
+            if user_id in self.conversations:
+                await self.save_transcript(user_id)
+                transcript = await self.load_transcript(user_id)
+                if transcript:
+                    transcript_file = io.BytesIO(transcript.encode('utf-8'))
+                    file = nextcord.File(transcript_file, filename=f"{user_id}_transcript.txt")
+                    embed = nextcord.Embed(
+                        title='✅ Transcript saved',
+                        description=f"Conversation with {user.name} has been saved and closed.",
+                        color=nextcord.Color.green(),
+                        timestamp=ctx.message.created_at
+                    )
+                    await ctx.send(embed=embed)
+
+                    # Send transcript to the specified channel
+                    await transcript_channel.send(f"Transcript for conversation with {user.name}:", file=file)
+                    
+                    # Close the conversation channel
+                    channel_name = f'modmail-{user_id}'
+                    mails_channel = nextcord.utils.get(ctx.guild.text_channels, name=channel_name)
+                    if mails_channel:
+                        await mails_channel.delete()
+                        del self.conversations[user_id]
+                else:
+                    embed = nextcord.Embed(
+                        title='❌ Failed to save transcript',
+                        description=f"Can't find user with the id {user_id}",
+                        color=nextcord.Color.red(),
+                        timestamp=ctx.message.created_at
+                    )
+                    await ctx.send(embed=embed)
             else:
                 embed = nextcord.Embed(
-                    title='❌ Failed to save transcript',
-                    description=f"Can't find user with the id {user_id}",
+                    title='❌ No active conversation',
+                    description=f"There is no active conversation with {user.name}",
                     color=nextcord.Color.red(),
                     timestamp=ctx.message.created_at
                 )
                 await ctx.send(embed=embed)
         else:
             embed = nextcord.Embed(
-                title='❌ Failed to save transcript',
-                description=f"Can't find user with the id {user_id}",
+                title='❌ Failed to end conversation',
+                description=f"Can't find user or transcript channel with the provided IDs",
                 color=nextcord.Color.red(),
                 timestamp=ctx.message.created_at
             )
             await ctx.send(embed=embed)
 
-        
 
-
-    @commands.command()
-    @commands.guild_only()
-    @has_permissions(manage_messages=True)
-    async def r(self, ctx, user: nextcord.Member = None, *, msg: str = "moderator is replying..."):
-        await ctx.trigger_typing()
-        message = msg
-        user = self.bot.get_user(user.id)
-        if user is not None:
-            # EMBED report result to user
-            embed_reply = nextcord.Embed(
-                title=f'🔔 Reply from Moderator || {ctx.author.name}',
-                description=f"```{message}```",
-                color=nextcord.Color.blue(),
-                timestamp=ctx.message.created_at
-            )
-            await user.send(embed=embed_reply)
-
-            # EMBED show results to author
-            embed_result = nextcord.Embed(
-                title='✅ Message sent successfully',
-                description=f"Message sent to {user.name}",
-                color=nextcord.Color.green(),
-                timestamp=ctx.message.created_at
-            )
-            await ctx.send(embed=embed_result)
-        elif user is None:
-            # EMBED show results to author
-            embed_result = nextcord.Embed(
-                title='❌ Failed to send message',
-                description=f"Can't find user with the id {id}",
-                color=nextcord.Color.red(),
-                timestamp=ctx.message.created_at
-            )
-            await ctx.send(embed=embed_result)
-
-
-
-    @r.error
-    async def reply_error(self, ctx, error):
-        if isinstance(error, BadArgument):
-            embed = nextcord.Embed(
-                title="That's not how you use it",
-                description="the correct format is <prefix>r <id> <message>",
-                color = nextcord.Color.red(),
-            )
-            await ctx.send(embed=embed)
-        else:
-            embed = nextcord.Embed(
-                title='Syntax error',
-                description="recheck your command.\nsee help module of the command for details",
-                color = nextcord.Color.red(),
-            )
-            await ctx.send(embed=embed)
     
-    @commands.command()
+    @commands.command(description="Loads a transcript for a user")
     @commands.guild_only()
     @has_permissions(manage_messages=True)
     async def gtrans(self, ctx, user_id: int):
@@ -245,7 +276,7 @@ class ModMail(commands.Cog):
         if transcript:
             # Send transcript as a file
             with io.StringIO(transcript) as fp:
-                await ctx.send(f"Transcript for user {user_id}:", file=nextcord.File(fp, f"{user_id}_transcript.txt"))
+                await ctx.send(f"Transcript for user {user_id}:", file=nextcord.File(fp, f"{user_id}.txt"))
         else:
             await ctx.send("Transcript not found.")
 
--- modmail.py
+++ modmail.py
@@ -5,27 +5,28 @@ from dotenv import load_dotenv
 from pretty_help import PrettyHelp
 from loguru import logger
 
+# this is for pretty help #
+
 ending_note = "For additional assistance, contact a moderator."
 color = 0x00FF00
 
-load_dotenv()
-TOKEN = os.getenv('BOT_TOKEN')
 
 
+# the bots intents #
 intents = nextcord.Intents.all()
 intents.guild_messages = True
 bot = commands.Bot(command_prefix='!', intents=nextcord.Intents.all(),help_command=PrettyHelp())
 
-
+# this is for the logger #
 logger.add("logs/file.log", level="TRACE")
 
-
+# this is the on ready event #
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} is online.')
     logger.info("Wadder ModMail is online.")
 
-
+# this is the on message event #
 @bot.event
 async def on_message(message):
     # Log the message
@@ -34,8 +35,8 @@ async def on_message(message):
     # Process the message as usual
     await bot.process_commands(message)
 
-
+# this is the start of the modmail bot #
 if __name__ == '__main__':
-    bot.load_extension('cogs.waddermodmail')  # Assuming the ModMail cog is in a file named "modmail.py"
-    bot.run(TOKEN)
+    bot.load_extension('cogs.waddermodmail') 
+    bot.run('MTA3NTE0NDgyODMzMDk3MTE4Ng.GoO7_2.KmJKfemVuGhl0yon9OT73Z-15ZNg_YC-qcmJu0')
 
